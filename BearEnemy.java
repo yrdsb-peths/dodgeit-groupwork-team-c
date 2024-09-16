a520bd71b4af62a551ee9939d913d91f5738e2c3
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-09-16 12:57:54 -0400

    Added a Comment

--- BearEnemy.java
+++ BearEnemy.java
@@ -8,10 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class BearEnemy extends Actor
 {
-    /**
-     * Act - do whatever the BearEnemy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    //This is the code that allows the Bear Enemy to move
     public void act()
     {
         move(-3);
